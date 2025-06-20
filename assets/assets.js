7851a842f9308427fe9a3324ad95baeca1471a8e
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-06-21 06:37:25 +0700

    Add About, Header, and Navbar components; update styles and layout; add assets and public files

--- assets/assets.js
+++ assets/assets.js
@@ -108,7 +108,7 @@ export const serviceData = [
 
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Python, R Project, C++, Jira, Kedro' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'System Information in Computer Science' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
